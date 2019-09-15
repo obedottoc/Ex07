b80bf456d4d6b9d0825750de950298c2c99bfc1c
SATHISH KUMAR <SATHISH KUMAR@DESKTOP-SC1E8E5>
2019-09-15 17:15:18 +0530

    created by kaarthikeyan

---
+++ src/fileinfo/FileInformation.java
@@ -0,0 +1,54 @@
+package fileinfo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileInformation {
+	
+	public void CheckFile()
+	{
+		Scanner sc=new Scanner(System.in);
+		String filename;
+		File f;
+		
+		System.out.println("Enter the name of a file with full path:");
+		filename=sc.next();
+		f=new File(filename);
+		
+		if(f.exists()==true)
+		{
+			System.out.println("The file exists.");
+			if(f.canRead())
+			{
+				System.out.println("The file is readable");
+			}
+			else {
+				System.out.println("The file is not readable");
+			}
+			
+			if(f.canWrite()) {
+				System.out.println("The file is write");
+			}
+			if(f.canExecute())
+			{
+				System.out.println("The file is Execute");
+			}
+			else {
+				System.out.println("The file is not Execute");
+			}
+			System.out.println("Length of the file:"+f.length());
+		}else
+		{
+			System.out.println("The file does not exist.");
+		}
+	}
+
+	public static void main(String[] args) {
+		FileInformation fi;
+		
+			fi=new FileInformation();
+			fi.CheckFile();
+
+	}
+
+}
