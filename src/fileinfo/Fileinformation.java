be001f4809b85c9e171814dbb1c449bba60adc95
user3 <user3@administrator-HP-Pro-3330-MT>
2019-09-16 09:16:16 +0530

    A Java program that reads a file name from the user, displays information about the file

---
+++ src/fileinfo/Fileinformation.java
@@ -0,0 +1,62 @@
+package fileinfo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Fileinformation {
+
+public void CheckFile()
+{
+Scanner sc=new Scanner(System.in);
+String filename;
+File f;
+
+System.out.println("Enter the name of a file with full path:");
+filename=sc.next();
+f=new File(filename);
+
+if(f.exists()==true)
+{
+System.out.println("The file exists.");
+
+
+if(f.canRead()==true)
+{
+System.out.println("The file can be read.");
+}else
+{
+System.out.println("The file cannot br read.");
+
+if(f.canWrite()==true)
+{
+System.out.println("The file can be written.");
+}else
+{
+System.out.println("The file cannot br written.");
+
+if(f.canExecute()==true)
+{
+System.out.println("The file type is executed.");
+}else
+{
+System.out.println("The file type is not excuted.");
+}
+}
+System.out.println("Length:"+f.length());
+}
+}
+
+else
+{
+System.out.println("the file does not exist.");
+}
+}
+public static void main(String[] args) {
+Fileinformation fi;
+
+fi=new Fileinformation();
+fi.CheckFile();
+
+}
+
+}
