b7e2c6be2be50695e382baa20ae513e5b191f051
user3 <user3@simlab27>
2019-09-16 20:26:05 +0530

    Ex.no-7, To know a file is readable,writable,

--- src/fileinfo/FileInformation.java
+++ src/fileinfo/FileInformation.java
@@ -1,10 +1,10 @@
 package fileinfo;
 
 import java.io.File;
-
 import java.util.Scanner;
 
 public class FileInformation {
+	
 	public void CheckFile()
 	{
 		Scanner sc=new Scanner(System.in);
@@ -18,16 +18,46 @@ public class FileInformation {
 		if(f.exists()==true)
 		{
 			System.out.println("The file exists.");
+		
+
+		if(f.canRead()==true)
+		{
+			System.out.println("The file can be read.");
 		}else
 		{
-			System.out.println("The file does not exist.");
+			System.out.println("The file cannot be read.");
 		}
+
+		if(f.canWrite()==true)
+		{
+			System.out.println("The file can be written.");
+		}else
+		{
+			System.out.println("The file cannot be written.");
+		}
+
+		if(f.canExecute()==true)
+		{
+			System.out.println("The file type is executed.");
+		}else
+		{
+			System.out.println("The file type is not executed.");
+		}
+	
+	}
+		else
+	{
+		System.out.println("The file does not exist.");
+	}
 	}
 	public static void main(String[] args) {
 		FileInformation fi;
 		
 		fi=new FileInformation();
 		fi.CheckFile();
-		}
 
-}
+	}
+	
+
+	}
+
