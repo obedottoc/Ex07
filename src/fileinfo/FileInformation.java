081fff601ed578d4f16a3c3888fef8940ecbd06f
user3 <user3@simlab29>
2019-09-16 10:16:10 +0530

    fileinformation

---
+++ src/fileinfo/FileInformation.java
@@ -0,0 +1,33 @@
+package fileinfo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileInformation {
+	public void CheckFile()
+	{
+		Scanner sc=new Scanner(System.in);
+		String filename;
+		File f;
+		
+		System.out.println("Enter the name of a file with full path:");
+		filename=sc.next();
+		f=new File(filename);
+		
+		if(f.exists()==true)
+		{
+			System.out.println("The file exists");
+		
+		}else
+		{ 
+			System.out.println("The file does not exist");
+			
+		}
+	}
+	public static void main(String[]args) {
+		FileInformation fi;
+		fi=new FileInformation();
+		fi.CheckFile();
+	}
+
+}
