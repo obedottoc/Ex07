9c11a1ab4f1fccde51e8b7a90b698000e8f5e119
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-19 09:33:06 +0530

    Created By Avinash Raja

---
+++ src/FileInfo/FileInformation.java
@@ -0,0 +1,69 @@
+package FileInfo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileInformation {
+
+public void CheckFile()
+{
+Scanner sc=new Scanner(System.in);
+String filename;
+File f;
+
+System.out.println("Enter the name of a file with full path:");
+filename=sc.next();
+f=new File(filename);
+
+if(f.exists()==true)
+{
+System.out.println("The file exists.");
+
+if(f.canRead()==true)
+{
+System.out.println("The file can be read.");
+
+}
+else
+{
+System.out.println("The file cannot be read.");
+}
+{
+if(f.canWrite()==true)
+{
+System.out.println("The file can be written.");
+}
+else
+{
+System.out.println("The file cannot be written.");
+}
+if(f.canExecute()==true)
+{
+System.out.println("The file type is executable");
+}
+else
+{
+System.out.println("The file type is not executable");
+}
+{
+
+System.out.println("Length:"+f.length());
+}
+ }
+    }
+else
+{
+System.out.println("The file does not exist.");
+}
+}
+
+
+public static void main(String[] args) {
+FileInformation fi;
+
+fi=new FileInformation();
+fi.CheckFile();
+
+}
+
+}
